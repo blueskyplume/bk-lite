2afb0db3c58e48138b5260c1cfa8b559d1f8c9a3
QiuJia-layla <1406489435@qq.com>
2026-04-07 14:53:15 +0800

    feat: fix validation failure issues

--- web/src/app/cmdb/(pages)/assetData/page.tsx
+++ web/src/app/cmdb/(pages)/assetData/page.tsx
@@ -146,6 +146,7 @@ interface ModelTabs {
 interface FieldRef {
   showModal: (config: {
     type: string;
+    source?: 'create' | 'copy' | 'edit' | 'batchEdit';
     attrList: FullInfoGroupItem[];
     formInfo: any;
     subTitle: string;
@@ -641,7 +642,7 @@ const AssetDataContent = () => {
     if (id) showInstanceModal({ _id: id });
   };
 
-  const showAttrModal = (type: string, row = {}) => {
+  const showAttrModal = (type: 'add' | 'edit' | 'batchEdit', row = {}) => {
     const title = type === 'add' ? t('common.addNew') : t('common.edit');
     fieldRef.current?.showModal({
       title,

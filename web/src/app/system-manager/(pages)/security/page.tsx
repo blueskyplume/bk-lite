d53b5e29f895c445d573c6d08ea1053908f94430
zhmf7408 <kido@canway.net>
2026-04-20 15:55:24 +0800

    feature 测试提交

--- web/src/app/system-manager/(pages)/security/page.tsx
+++ web/src/app/system-manager/(pages)/security/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRedirectFirstChild } from '@/hooks/useRedirectFirstChild';
+import { useUserApi } from '@/app/system-manager/api/user/index';
 
 export default function UserPage() {
   useRedirectFirstChild();
